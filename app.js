4f4c1052b77a0b5be06c523691b970565884d363
Jesse Brink <jbrink0925@gmail.com>
2021-11-25 12:59:51 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const { ExpressError, NotFoundError } = require("./expressError");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+    return next(new NotFoundError());
+});
+
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status }
+    });
+});
+
+module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = new express.Router();
+const jsonschema = require("jsonschema");
+const { createToken } = require("../helpers/tokens");
+const { BadRequestError } = require("../expressError");
+const userSchema = require("../schemas/userSchema.json");
+
+router.post("/signup", async function(req, res, next) {
+    try {
+        const validator = jsonschema.validate(req.body, userSchema);
+        if (!validator.valid) {
+            const errors = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errors);
+        }
+        const newUser = User.register(req.body);
+        const token = createToken(newUser);
+        return res.status(201).json({ token });
+    } catch (error) {
+        return next(error);
+    }
+});
+
+router.post("/login", async function(req, res, next) {
+    try {
+        const validator = jsonschema.validate(req.body, userSchema);
+        if (!validator.valid) {
+            const errors = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errors);
+        }
+        const user = User.authenticate(req.body);
+        const token = createToken(user);
+        return res.json({ token });
+    } catch (error) {
+        return next(error);
+    }
+});
