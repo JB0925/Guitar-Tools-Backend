f3755cf117b124f06d33c5c33a6637c564801633
Jesse Brink <jbrink0925@gmail.com>
2021-11-27 23:24:15 -0500

    add route for getting information about famous guitarists and musicians from AudioDB API.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { AuthenticateJWT } = require("./helpers/tokens");
 const authRoutes = require("./routes/auth");
+const guitaristRoute = require("./routes/guitarists");
 const { ExpressError, NotFoundError } = require("./expressError");
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(AuthenticateJWT);
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
+app.use("/guitarist", guitaristRoute);
 
 app.use((req, res, next) => {
     return next(new NotFoundError());
---
+++ routes/guitarists.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = new express.Router();
+const axios = require("axios");
+const { AUDIODB_API_KEY } = require("../config");
+const { BadRequestError } = require("../expressError");
+
+router.get("/:name", async(req, res, next) => {
+    let { name } = req.params;
+    name = name.split("_").join(" ");
+    const options = {
+        method: "GET",
+        url: 'https://theaudiodb.p.rapidapi.com/search.php',
+        params: { s: name },
+        headers: {
+            'x-rapidapi-host': 'theaudiodb.p.rapidapi.com',
+            'x-rapidapi-key': AUDIODB_API_KEY
+        }
+    };
+    
+    try {
+        const response = await axios.request(options);
+        if (!response.data.artists) throw new BadRequestError("Sorry, we could not find this musician.");
+
+        const { intBornYear, intDiedYear, strGenre, strBiographyEN, strArtistThumb } 
+            = response.data.artists[0];
+
+        return res.json({
+            birthYear: intBornYear,
+            deathYear: intDiedYear === null ? "N/A" : intDiedYear,
+            genre: strGenre,
+            biography: strBiographyEN,
+            photo: strArtistThumb
+        });
+    } catch (error) {
+        return next(error);
+    };
+});
+
+module.exports = router;
