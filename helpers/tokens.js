189167a6ba9d5f177c45bf429482a78ced157f36
Jesse Brink <jbrink0925@gmail.com>
2021-12-31 18:34:44 -0500

    update files for deployment

--- helpers/tokens.js
+++ helpers/tokens.js
@@ -1,10 +1,11 @@
 const jwt = require("jsonwebtoken");
 // const { SECRET_KEY } = process.env.SECRET_KEY || require("../config");
-if (process.env.NODE_ENV === "production") {
-  const SECRET_KEY = process.env.SECRET_KEY;
-} else {
-  const { SECRET_KEY } = require("../config");
-}
+// if (process.env.NODE_ENV === "production") {
+//   const SECRET_KEY = process.env.SECRET_KEY;
+// } else {
+//   const { SECRET_KEY } = require("../config");
+// }
+const SECRET_KEY = process.env.SECRET_KEY;
 
 /** return signed JWT from user data. */
 
