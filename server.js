6efe0150965b3e42c0929f54e249bb07c3e78303
Jesse Brink <jbrink0925@gmail.com>
2021-12-31 18:08:53 -0500

    update files for deployment

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const app = require("./app");
-const { PORT } = process.env.PORT || require("./config");
+// const { PORT } = +process.env.PORT || require("./config");
+const PORT = +process.env.PORT || 3001;
 
 app.listen(PORT, function () {
   console.log(`Started on http://localhost:${PORT}`);
